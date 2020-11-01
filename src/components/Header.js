4fd73a1fbb5fcae658adb50f106b13948fc8f3aa
ejiwen <skibnewrim@gmail.com>
2020-10-31 18:40:12 -0700

    install material-ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCodepen, faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
 
 const Header = () => {
    return (
-       <header>
+    <header>
+    <div>LOGO</div>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Writing</li>
+            <li>Work</li>
+            <li>Résumé</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    {/* <div className="cnt-social-media">
+    <FontAwesomeIcon icon={faLinkedin} size="lg" />
+    <FontAwesomeIcon icon={faGithubSquare} size="lg"  />
+    <FontAwesomeIcon icon={faDev} size="lg"  />
+    </div> */}
+    </header> 
+   )
+}
+
+export default Header;
+
+
+
+/*
+
+<header>
        <div>LOGO</div>
        <nav>
            <ul>
@@ -21,7 +56,5 @@ const Header = () => {
        <FontAwesomeIcon icon={faDev} size="lg"  />
        </div>
        </header> 
-   )
-}
 
-export default Header;
+*/
