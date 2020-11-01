40fd58d3771ba0294f50831354785b493da451ae
ejiwen <skibnewrim@gmail.com>
2020-10-31 19:41:51 -0700

    create the header ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,34 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button, Container} from 'react-bootstrap'
 
 const Header = () => {
    return (
-    <header>
-    <div>LOGO</div>
-    <nav>
-        <ul>
-            <li>Home</li>
-            <li>Writing</li>
-            <li>Work</li>
-            <li>Résumé</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-    {/* <div className="cnt-social-media">
-    <FontAwesomeIcon icon={faLinkedin} size="lg" />
-    <FontAwesomeIcon icon={faGithubSquare} size="lg"  />
-    <FontAwesomeIcon icon={faDev} size="lg"  />
-    </div> */}
-    </header> 
+    <Container>
+  <Navbar collapseOnSelect expand="lg" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar></Container>
    )
 }
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button, Container} from 'react-bootstrap'
 
 
 const Layout = ({children}) => {
  return (
-     <div className="container">
+    <Container fluid>
          <Header />
          <p> {children} </p>
          <h1> FOOTER </h1>
-     </div>
+         </Container>
  )
 }
 
