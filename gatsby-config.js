cb7c3a119f33de899f1c032a0bf3217c1cb86a34
ejiwen <skibnewrim@gmail.com>
2020-11-18 23:48:48 -0800

    MDX first step

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,19 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-fontawesome-css`,`gatsby-transformer-sharp`, `gatsby-plugin-sharp`,
+    `gatsby-plugin-fontawesome-css`,`gatsby-transformer-sharp`, `gatsby-plugin-sharp`,`gatsby-plugin-mdx`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
   ],
--- src/components/Writing.js
+++ src/components/Writing.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { graphql } from 'gatsby'
 
-const Writing = () => {
+const Writing = ({data}) => {
+    console.log(data);
     return(
         <div className="writing">
             test
             <img src="images/writing1.svg" />
+            <div>
+
+            </div>
         </div>
     )
 }
+
+
+export const query = graphql`
+  {
+    allMdx(sort: {order: DESC, fields: frontmatter___date}, limit: 2) {
+      nodes {
+        excerpt
+        frontmatter {
+          author
+          date(fromNow: true)
+          slug
+          title
+          category
+          readTime
+          
+        }
+      }
+    }
+  }
+`
 
 export default Writing;
