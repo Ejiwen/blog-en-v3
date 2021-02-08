b0f2bf769e30bd5b20563fba9178d1e4589920d9
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2021-02-07 21:09:03 -0800

    Update ProjetDisc.js

--- src/components/parts/ProjetDisc.js
+++ src/components/parts/ProjetDisc.js
@@ -1,4 +1,2 @@
-export const MNTIC = `MNTIC has provided our clients with information, technology, and business solutions that address critical 
-challenges. Our collaborative approach fosters long-term relationships and trust, which allows us to develop
-strategic insights that maximize investments and competitive advantage.`;
+export const MNTIC = ``;
 export const IRBEH = ``
