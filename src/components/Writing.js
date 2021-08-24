133d5a92e5b7b8ecb9c9eaaf775be1e67f83e7d7
ejiwen <skibnewrim@gmail.com>
2021-08-24 11:04:16 -0700

    Minor changes

--- src/components/Writing.js
+++ src/components/Writing.js
@@ -1,30 +1,27 @@
-import React from 'react'
-import Post from './Post'
+import React from "react"
+import Post from "./Post"
 import headerWriting from "../../static/images/writingBg.svg"
 
-const Writing = ({allMdx:{nodes}}) => {
-  
-    console.log("-----------");
-    console.log(nodes);
-    console.log("-----------");
-    return(
-        <div className="wrap-writing">
-        
-        <div className="writing">
+const Writing = ({ allMdx: { nodes } }) => {
+  console.log("-----------")
+  console.log(nodes)
+  console.log("-----------")
+  return (
+    <div className="wrap-writing">
+      <div className="writing">
         <div className="writing__header">
-            <img src={headerWriting} /><h3> Recent Posts </h3>
-            </div>
-            <div className="writing__body">
-            {/* <img src="images/writing1.svg" />    */}
-             {
-             nodes.map(post => <Post key={post.id} {...post} />)
-             }
+          <img src={headerWriting} />
+          <h3> My Recent Posts </h3>
         </div>
+        <div className="writing__body">
+          {/* <img src="images/writing1.svg" />    */}
+          {nodes.map(post => (
+            <Post key={post.id} {...post} />
+          ))}
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-
-
-export default Writing;
+export default Writing
