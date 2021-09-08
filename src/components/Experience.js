f8ab5c8c83e5cd209f5ea4296e31877d789e3bf2
ejiwen <skibnewrim@gmail.com>
2021-09-08 11:45:29 -0700

    add curver mask to section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,105 +1,96 @@
-import React, { useState } from 'react'
-import ExperienceTmpl from './parts/ExperienceTmpl';
-import styled from '@emotion/styled'
+import React, { useState } from "react"
+import ExperienceTmpl from "./parts/ExperienceTmpl"
+import styled from "@emotion/styled"
 import headerExperiences from "../../static/images/experiencesBg.svg"
 
 const ExperienceBttomBg = styled.div`
-background:url('https://assets-global.website-files.com/5eb2825fc062423a537b975c/5eb2825fc06242394c7b9980_mask.svg') no-repeat;
-background-size: cover;
-height: 36px;
+  background: url("sectionmask3.svg") no-repeat;
+  background-size: cover;
+  height: 72px;
 `
 
 const Experience = () => {
+  const experiencesArray = [
+    {
+      company: "HACKREACTOR",
+      role: "Software Engeneer",
+      periode: "2021 - ",
+      experience: [],
+    },
+    {
+      company: "KINROSS GOLD",
+      role: "Autodesk Data Analyst",
+      periode: "Nov 2018 - Jan 2020",
+      experience: [
+        "Using AutoDesk vault to Provide engineering data and analysis.",
+        "Experience in data analysis with the ability to understand and interpret data.",
+        "Development of our internal big data solutions as well as the related data visualization suite.",
+        "Experience in using databases, the SQL language, and the principles of data normalization.",
+        "Collaborate with wider groups on standards and best practices for Big Data / Monitoring Software related areas",
+      ],
+    },
+    {
+      company: "MNTIC",
+      role: "Software Engineer",
+      periode: "Jan 2017 - Jan 2018",
+      experience: [
+        "Develop highly detailed style guides using a combination of HTML, CSS, TypeScript, JavaScript and using JavaScript frameworks like Backbone and ReactJS.",
+        " Working with and building RESTful API's and GraphQL.",
+        "Lead and participate in new feature design and code reviews.",
+        "Experience with Java development frameworks such as Spring.",
+        "Fixing bugs, Suggesting improvements.",
+        "Organized and reliable, attentive to detail, able to efficiently manage multiple tasks.",
+      ],
+    },
+    {
+      company: "HADMOOL",
+      role: "Full Stack Developer",
+      periode: "Jan 2016 - Jan 2017",
+      experience: [
+        "Experience with PHP/MySQL and WordPress.",
+        "Proficient using web services, ajax, and JSON.",
+        "Experience with web markup, including HTML5, CSS3, JavaScript.",
+        "A good working knowledge of the Adobe Creative Suite (Photoshop and Illustrator in particular).",
+        "Practical skills using the following tools: Gulp, WebPack, Babel.",
+      ],
+    },
+  ]
 
-    const experiencesArray = [
-      {
-        company: 'HACKREACTOR',
-        role: "Software Engeneer",
-        periode: "2021 - ",
-        experience: [
-         
-        ],
+  const [cmps, setCmps] = useState(experiencesArray[1])
 
-      },
-        {
-          company: 'KINROSS GOLD',
-          role: "Autodesk Data Analyst",
-          periode: "Nov 2018 - Jan 2020",
-          experience: [
-            "Using AutoDesk vault to Provide engineering data and analysis.",
-            "Experience in data analysis with the ability to understand and interpret data.",
-            "Development of our internal big data solutions as well as the related data visualization suite.",
-            "Experience in using databases, the SQL language, and the principles of data normalization.",
-            "Collaborate with wider groups on standards and best practices for Big Data / Monitoring Software related areas"
-          ],
+  const toggleCMP = index => {
+    setCmps(experiencesArray[index])
+  }
 
-        },
-        {
-          company: 'MNTIC',
-          role: "Software Engineer",
-          periode: "Jan 2017 - Jan 2018",
-          experience: [
-            "Develop highly detailed style guides using a combination of HTML, CSS, TypeScript, JavaScript and using JavaScript frameworks like Backbone and ReactJS.",
-          " Working with and building RESTful API's and GraphQL.",
-          "Lead and participate in new feature design and code reviews.",
-          "Experience with Java development frameworks such as Spring.",
-          "Fixing bugs, Suggesting improvements.",
-          "Organized and reliable, attentive to detail, able to efficiently manage multiple tasks."
-        ],
-
-        },
-        {
-          company: 'HADMOOL',
-          role: "Full Stack Developer",
-          periode: "Jan 2016 - Jan 2017",
-          experience: [
-            'Experience with PHP/MySQL and WordPress.',
-            'Proficient using web services, ajax, and JSON.',
-            'Experience with web markup, including HTML5, CSS3, JavaScript.',
-            'A good working knowledge of the Adobe Creative Suite (Photoshop and Illustrator in particular).',
-            'Practical skills using the following tools: Gulp, WebPack, Babel.'
-          ],
-
-        }
-      ];
-
-    const [cmps, setCmps] = useState(experiencesArray[1]);
-
-      const toggleCMP = index => { 
-        setCmps(experiencesArray[index])
-        }
-
-    return (
-        <div id="Experiencesy" className="experience-wrap">
-          <ExperienceBttomBg className="topExpBg"></ExperienceBttomBg>
-        <div className="experience">
-            <div className="experience__header">
-            
-            <img src={headerExperiences} /> <h3> Experiences </h3>
+  return (
+    <div id="Experiencesy" className="experience-wrap">
+      <ExperienceBttomBg className="topExpBg"></ExperienceBttomBg>
+      <div className="experience">
+        <div className="experience__header">
+          <img src={headerExperiences} /> <h3> Experiences </h3>
         </div>
         <div className="experience__body">
-            <div className="menu">
-                <ul>
-                   {
-                       experiencesArray.map((cmp,i) => (
-                       <li onClick={() => toggleCMP(i)} key={i}>{cmp.company}</li>
-                       ))
-                   }
-                </ul>
-            </div>
+          <div className="menu">
+            <ul>
+              {experiencesArray.map((cmp, i) => (
+                <li onClick={() => toggleCMP(i)} key={i}>
+                  {cmp.company}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-            <div className="content">              
-               <ExperienceTmpl {...cmps} /> 
-            </div>
+          <div className="content">
+            <ExperienceTmpl {...cmps} />
+          </div>
         </div>
-        </div>
-        <ExperienceBttomBg></ExperienceBttomBg>
-        </div>
-    )
+      </div>
+      <ExperienceBttomBg></ExperienceBttomBg>
+    </div>
+  )
 }
 
-export default Experience;
-
+export default Experience
 
 /*
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import Navbar from './Navbar'
-import styled from '@emotion/styled'
-import {bgHeader} from "../../static/images/header-mr-sf.svg"
+import React from "react"
+import Navbar from "./Navbar"
+import styled from "@emotion/styled"
+import { bgHeader } from "../../static/images/header-mr-sf.svg"
 
 const HeaderBttomBg = styled.div`
-background:url('https://assets-global.website-files.com/5eb2825fc062423a537b975c/5eb2825fc06242394c7b9980_mask.svg') no-repeat;
-background-size: cover;
-height: 36px;
+  background: url("sectionmask3.svg") no-repeat;
+  background-size: cover;
+  height: 72px;
 `
 
-
 const Header = () => {
-   return (
+  return (
     <header>
-       <Navbar />
-       <div className="my-bio-container">
-       <div className="my-bio">
-           <div>
-           <p> Hi, My name is </p>
-           <h3>CHEIKHANY EJIWEN</h3>
-           <p className="my-bio--role">Software Engineer in san francisco</p>
-           </div>
-       </div>
-       </div>
-       <img src={bgHeader} style={{visibility: "hidden", height:"600px"}} />
-       <HeaderBttomBg></HeaderBttomBg>
+      <Navbar />
+      <div className="my-bio-container">
+        <div className="my-bio">
+          <div>
+            <p> Hi, My name is </p>
+            <h3>CHEIKHANY EJIWEN</h3>
+            <p className="my-bio--role">Software Engineer in san francisco</p>
+          </div>
+        </div>
+      </div>
+      <img src={bgHeader} style={{ visibility: "hidden", height: "600px" }} />
+      <HeaderBttomBg></HeaderBttomBg>
     </header>
-
-   )
+  )
 }
 
-export default Header;
+export default Header
 
 /*
         import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
